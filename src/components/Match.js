ed99e9c08bfa08f7dcc2b4a2070f8ae80e095177
yann <hernandezyann@gmail.com>
2021-02-19 00:52:05 +0100

    filters finis

--- src/components/Match.js
+++ src/components/Match.js
@@ -1,111 +1,139 @@
-import React, {useState, useEffect, useImperativeHandle} from 'react';
-import axios from 'axios'
+import React, { useState, useEffect, useImperativeHandle } from "react"
+import axios from "axios"
 
-import Selection from './Selection'
+import Selection from "./Selection"
 
-import './Match.css'
+import "./Match.css"
 
 function Match() {
-    const [persos, setPersos] = useState([])
-    const [filterPerso, setFilterPerso] = useState('')
-    const [hair, setHair] = useState('')
-    const [species, setSpecies] = useState('')
-    const [eye, setEye] = useState('')
-    
-    useEffect(() => {
-        axios.get('https://miadil.github.io/starwars-api/api/all.json')
-            .then((res) => setPersos(res.data))
-    }, []);
+	const [persos, setPersos] = useState([])
+	const [filteredPersos, setFilteredPersos] = useState([])
+	const [filterPerso, setFilterPerso] = useState("")
+	const [hair, setHair] = useState("")
+	const [species, setSpecies] = useState("")
+	const [eye, setEye] = useState("")
 
-    useEffect(() => {
-        setFilterPerso(persos.filter(perso => perso.hairColor === hair))
-        console.log(filterPerso.filter(perso => perso.hairColor === hair))
-    }, [hair, species, eye]);
+	useEffect(() => {
+		axios
+			.get("https://miadil.github.io/starwars-api/api/all.json")
+			.then((res) => setPersos(res.data))
+	}, [])
 
-    const handleChoice = (e, choice) => {
-        if (choice === 'hair'){
-            setHair(e.target.value) 
-        } else if (choice === 'eye'){
-            setEye(e.target.value)
-        } else if (choice === 'species'){
-            setSpecies(e.target.value)
-        } 
-        console.log(filterPerso)
+	const filterFunction = (filterArray, dataArray) => {
+		if (!filterArray[0]) return dataArray
+		const filteredData = dataArray.filter((data) => {
+			let missingFilter = false
+			const allFilterOk = filterArray.filter((filter, index) => {
+				if (missingFilter) return false
+				if (data[filter.name] !== filter.value) {
+					missingFilter = true
+					return false
+				}
+				if (!filterArray[index + 1] && data[filter.name] === filter.value) {
+					return true
+				}
+			})
 
-        // if( hair !== '' && eye !== '' && species !== '') {
+			if (allFilterOk[0]) {
+				return true
+			} else return false
+		})
+		return filteredData
+	}
 
-        // }
-    }
-    return (
-        <div>
-            <h1> Quels sont tes matchs dans la galaxie ?</h1>
-       
-           <select name="hairColor" id="hair-select" onChange={(e) =>  handleChoice(e, 'hair') }>
-        <option value="">--Please choose an option--</option>
-        <option value="black">Black</option>
-        <option value="black  later with flecks of gray">black  later with flecks of gray</option>
-        <option value="auburn">Auburn</option>
-        <option value="auburn later gray">Auburn, Later Gray</option>
-        <option value="brown">Brown</option>
-        <option value="brown later gray">Brown, later gray</option>
-        <option value="brown-black">Brown-black</option>
-        <option value="light brown">Light Brown</option>
-        <option value="blond">Blond</option>
-        <option value="white">White</option>
-        <option value="gold">Gold</option>
-        <option value="red later gray">Red, later gray</option>
-    </select>
+	useEffect(() => {
+		const filterArray = []
+		if (hair) filterArray.push({ name: "hairColor", value: hair })
+		if (species) filterArray.push({ name: "species", value: species })
+		if (eye) filterArray.push({ name: "eyeColor", value: eye })
+		const filteredValue = filterFunction(filterArray, persos)
+		setFilteredPersos(filteredValue)
+	}, [persos, hair, species, eye])
 
-    <select name="Eye color" id="eye-color" onChange={(e) => handleChoice(e, 'eye')}>
-        <option value="">--Please choose an option--</option>
-        <option value="blue">blue</option>
-        <option value="red">red</option>
-        <option value="brown">brown</option>
-        <option value="hazel">hazel</option>
-        <option value="purple">purple</option>
-        <option value="orange">orange</option>
-        <option value="green">green</option>
-        <option value="black">black</option>
-        <option value="yellow">yellow</option>
-        <option value="pink">pink</option>
-        <option value="gold">gold</option>
-        <option value="white">white</option>
-    </select>
+	const handleChoice = (e, choice) => {
+		if (choice === "hair") {
+			setHair(e.target.value)
+		} else if (choice === "eye") {
+			setEye(e.target.value)
+		} else if (choice === "species") {
+			setSpecies(e.target.value)
+		}
+		console.log(filterPerso)
+	}
+	return (
+		<div>
+			<h1> Quels sont tes matchs dans la galaxie ?</h1>
 
-    <select name="speciesName" id="species" onChange={(e) => handleChoice(e, 'species')}>
-        <option value="">--Please choose an option--</option>
-        <option value="human">Human</option>
-        <option value="droid">Droid</option>
-        <option value="wookiee">Wookiee</option>
-        <option value="rodian">Rodian</option>
-        <option value="yoda">yoda's species</option>
-        <option value="trandoshan">Trandoshan</option>
-        <option value="mon calamari">Mon calamari</option>
-        <option value="ewok">Ewok</option>
-        <option value="sullustan">Sullustan</option>
-        <option value="neimoidian">Red, later gray</option>
-        <option value="gungan">Gungan</option>
-        <option value="toydarian">Toydarian</option>
-        <option value="dug">Dug</option>
-        <option value="dathomirian zabrak">dathomirian zabrak</option>
-        <option value="twi'lek">Twi'lek</option>
-    </select>
-{/*       
-    const [hair, setHair] = useState('')
-    const [species, setSpecies] = useState('')
-    const [eye, setEye] = useState('') */}
-        {/* {
-            hair ? persos.filter(perso => perso.hairColor === hair).map(perso =>  <div>{perso.name}</div>) : 'select the hair' 
-        } */}
-    {/* {persos.filter(perso => perso.hairColor === hair).filter(perso => perso.eyeColor === eye)
-    .map(perso => <div key={perso.id}>{perso.name}</div>)} */}
+			<select
+				name="hairColor"
+				id="hair-select"
+				onChange={(e) => handleChoice(e, "hair")}
+			>
+				<option value="">--Please choose an option--</option>
+				<option value="black">Black</option>
+				<option value="black  later with flecks of gray">
+					black later with flecks of gray
+				</option>
+				<option value="auburn">Auburn</option>
+				<option value="auburn later gray">Auburn, Later Gray</option>
+				<option value="brown">Brown</option>
+				<option value="brown later gray">Brown, later gray</option>
+				<option value="brown-black">Brown-black</option>
+				<option value="light brown">Light Brown</option>
+				<option value="blond">Blond</option>
+				<option value="white">White</option>
+				<option value="gold">Gold</option>
+				<option value="red later gray">Red, later gray</option>
+			</select>
 
-    {/* {hair !== ' ' ? persos.filter(perso => perso.hairColor === hair)
-    .map(perso => <div key={perso.id}>{perso.name}</div>) 
-    : hair == false ? persos.map(perso => (<div  key={perso.id}>{perso.name}</div>)): 'ok' } */}
-   
-       </div> 
-    )
+			<select
+				name="Eye color"
+				id="eye-color"
+				onChange={(e) => handleChoice(e, "eye")}
+			>
+				<option value="">--Please choose an option--</option>
+				<option value="blue">blue</option>
+				<option value="red">red</option>
+				<option value="brown">brown</option>
+				<option value="hazel">hazel</option>
+				<option value="purple">purple</option>
+				<option value="orange">orange</option>
+				<option value="green">green</option>
+				<option value="black">black</option>
+				<option value="yellow">yellow</option>
+				<option value="pink">pink</option>
+				<option value="gold">gold</option>
+				<option value="white">white</option>
+			</select>
+
+			<select
+				name="speciesName"
+				id="species"
+				onChange={(e) => handleChoice(e, "species")}
+			>
+				<option value="">--Please choose an option--</option>
+				<option value="human">Human</option>
+				<option value="droid">Droid</option>
+				<option value="wookiee">Wookiee</option>
+				<option value="rodian">Rodian</option>
+				<option value="yoda">yoda's species</option>
+				<option value="trandoshan">Trandoshan</option>
+				<option value="mon calamari">Mon calamari</option>
+				<option value="ewok">Ewok</option>
+				<option value="sullustan">Sullustan</option>
+				<option value="neimoidian">Red, later gray</option>
+				<option value="gungan">Gungan</option>
+				<option value="toydarian">Toydarian</option>
+				<option value="dug">Dug</option>
+				<option value="dathomirian zabrak">dathomirian zabrak</option>
+				<option value="twi'lek">Twi'lek</option>
+			</select>
+
+			{filteredPersos?.map((perso) => (
+				<p>{perso?.name}</p>
+			))}
+		</div>
+	)
 }
 
-export default Match;
+export default Match
