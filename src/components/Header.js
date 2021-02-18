ed89fb3bf3a89e5a98f47c712bd8a8aa0f98e479
yann <hernandezyann@gmail.com>
2021-02-18 10:53:26 +0100

    create header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
+import {NavLink} from 'react-router-dom'
 
  class Header extends Component {
     render() {
         return (
             <div>
+                <header>
+                    <ul>
+                        <li>
+                            <NavLink></NavLink>
+                        </li>
+                        <li>
+                        <NavLink></NavLink>
+                        </li>
+                    </ul>
+                </header>
+
                 
             </div>
         )
