0cefb60650b49796e547cdbb8f72a145586a43b3
yann <hernandezyann@gmail.com>
2021-02-18 13:14:06 +0100

    initialisation du header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import {Switch, Route} from 'react-router-dom'
+import Home from './screen/Home'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Switch>
-        <Route exact path='/' component={}/>
-        <Route exact path='/' component={}/>
-        <Route path='//:id' component={}/>
+        <Route exact path='/' component={Home}/>
+        
       </Switch>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,32 @@
-import React, { Component } from 'react'
-import {NavLink} from 'react-router-dom'
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import './Header.css'
 
- class Header extends Component {
-    render() {
-        return (
-            <div>
-                <header>
-                    <ul>
-                        <li>
-                            <NavLink></NavLink>
-                        </li>
-                        <li>
-                        <NavLink></NavLink>
-                        </li>
-                    </ul>
-                </header>
-
-                
-            </div>
-        )
-    }
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <ul>
+            <li>
+              <NavLink to="/" className="nav" activeClassName="selected">
+                Home
+              </NavLink>
+            </li>
+            <li className='header'>
+              <NavLink to="/" className="nav" activeClassName="selected">
+                Find Your Match
+              </NavLink>
+            </li>
+            <li className='header'>
+              <NavLink to="/" className="nav" activeClassName="selected">
+                Training
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
-export default Header
+export default Header;
